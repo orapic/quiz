200757109a415c213fb1adc8fec3648f4a6a38fb
orapic <alex_5945@hotmail.com>
2015-05-15 16:40:42 +0200

    Borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -96,3 +96,10 @@ exports.update = function(req,res){
     } // Redirección HTTP a lista de preguntas (URL relativo)
   });
 };
+
+// DELETE quizes/:id
+exports.destroy = function(req,res) {
+  req.quiz.destroy().then( function(){
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
