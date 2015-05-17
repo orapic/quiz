7fc5ec67189e4504c6b3d5451a4ac226ec54a076
orapic <alex_5945@hotmail.com>
2015-05-17 23:18:30 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res) {
 // Autoload de comandos con :quizId
 // Sólo se ejecuta si hay parametros, si no los hay no lo hace
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login' , sessionController.new); // formulario login
@@ -32,6 +33,7 @@ router.delete('/quizes/:quizId(\\d+)' , sessionController.loginRequired, quizCon
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments' , commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish' , sessionController.loginRequired, commentController.publish); // uso no correcto, debería ser un put
 
 //página de créditos
 router.get('/author', function(req,res){
