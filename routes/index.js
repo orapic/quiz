84675f1868247b006c4f64c50298684ebdea4765
orapic <alex_5945@hotmail.com>
2015-05-17 20:27:40 +0200

    Autenticacion de usuarios

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -14,7 +15,12 @@ router.get('/', function(req, res) {
 // Sólo se ejecuta si hay parametros, si no los hay no lo hace
 router.param('quizId', quizController.load); // autoload :quizId
 
-// Definicion de ruta de quizes
+// Definición de rutas de sesión
+router.get('/login' , sessionController.new); // formulario login
+router.post('/login', sessionController.create); // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
+
+// Definicion de rutas de quizes
 router.get('/quizes', 	quizController.index);
 router.get('/quizes/:quizId(\\d+)' , quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer' , quizController.answer);
